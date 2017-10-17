279be32d2a7f1b69634124fb24b1894573984507
David Thames <davidct@vt.edu>
2017-10-16 21:29:43 -0400

    Comparable2D class and Direction Enum

---
+++ src/Comparable2D.java
@@ -0,0 +1,45 @@
+
+public class Comparable2D {
+	int x = 0;
+	int y = 0;
+	
+	/**
+	 * Get the x position of this object
+	 * 
+	 * @return x position
+	 */
+	int getX() {
+		return 0;
+	}
+	
+	/**
+	 * Get the y position of this object
+	 * 
+	 * @return y position
+	 */
+	int getY() {
+		return 0;
+	}
+	
+	
+	/**
+	 * Compare to another 2D comparable object
+	 * 
+	 * @param obj - object to compare with this object
+	 * @return direction based on x & y of both objects
+	 */
+	Direction compareTo(Object obj) {
+		return null;
+	}
+	
+	/**
+	 * Compare x and y coordinates
+	 * 
+	 * @param otherX - x coordinate to compare against
+ 	 * @param otherY - y coordinate to compare against
+	 * @return direction from the point (otherX, otherY)
+	 */
+	Direction compareTo(int otherX, int otherY) {
+		return null;
+	}
+}
---
+++ src/Direction.java
@@ -0,0 +1,4 @@
+
+public enum Direction {
+	NW, NE, SW, SE
+}
