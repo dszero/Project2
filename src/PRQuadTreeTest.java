2cf04b98e84f41038544328d1a0a1b743e5f901f
David Thames <davidct@vt.edu>
2017-10-21 17:38:20 -0400

    Updated PRQuadTree test casese

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -14,6 +14,8 @@ public class PRQuadTreeTest extends TestCase {
 	private Point point3;
 	private Point point4;
 	
+	private Point point5;
+	
 	public void setUp() {
 		tree = new PRQuadTree<Point>(0, 0, 1024, 1024);
 		
@@ -26,6 +28,8 @@ public class PRQuadTreeTest extends TestCase {
 		
 		point3 = new Point("p3", 200, 60);
 		point4 = new Point("p4", 235, 2);
+		
+		point5 = new Point("p5", 25, 29);
 	}
 	
 	public void testInsert() {
@@ -55,11 +59,85 @@ public class PRQuadTreeTest extends TestCase {
 	}
 	
 	public void testRemove() {
+		tree.insert((Point) null);
+		assertFalse(tree.remove((Point) null));
+		
+		tree.insert(point1);
+		assertFalse(tree.remove(point3));
+		assertTrue(tree.remove(point1));
+		tree.insert(point1);
+		
+		tree.insert(point2a);
+		assertFalse(tree.remove(point3));
+		assertTrue(tree.remove(point1));
+		tree.insert(point1);
+		assertTrue(tree.remove(point2a));
+		tree.insert(point2a);
+		
+		tree.insert(point2b);
+		tree.insert(point2c);
+		tree.insert(point2d);
+		assertTrue(tree.remove(point2c));
+		tree.insert(point2c);
+		
+		tree.insert(point1);
+		assertTrue(tree.remove(point1));
+		assertFalse(tree.remove(point1));
+		tree.insert(point2c);
+		
+		tree.insert(point3);
+		tree.insert(point4);
+		
+		assertTrue(tree.remove(point3));
+		assertTrue(tree.remove(point4));
+	}
+	
+	public void testRemoveXY() {
 		
 	}
 	
 	public void testFind() {
+		tree.insert((Point) null);
+		assertNull(tree.find((Point) null));
 		
+		tree.insert(point1);
+		assertNull(tree.find(point3));
+		assertEquals(tree.find(point1), point1);
+		tree.insert(point1);
+		
+		tree.insert(point2a);
+		assertNull(tree.find(point3));
+		assertEquals(tree.find(point1), point1);
+		tree.insert(point1);
+		assertEquals(tree.find(point2a), point2a);
+		tree.insert(point2a);
+		
+		tree.insert(point2b);
+		tree.insert(point2c);
+		tree.insert(point2d);
+		assertEquals(tree.find(point2c));
+		tree.insert(point2c);
+		
+		tree.insert(point1);
+		assertEquals(tree.find(point1), point1);
+		assertEquals(tree.find(point1), point1);
+		tree.insert(point2c);
+		
+		tree.insert(point3);
+		tree.insert(point4);
+		
+		assertEquals(tree.find(point3), point3);
+		assertEquals(tree.find(point4), point4);
+		assertNull(tree.find(point5));
+		
+		//test after removes
+		tree.remove(point1);
+		tree.remove(point2a);
+		tree.remove(point3);
+		assertNull(tree.find(point1));
+		assertNull(tree.find(point2a));
+		assertEquals(tree.find(point2c), point2c);
+		assertEquals(tree.find(point4), point4);
 	}
 	
 	public void testClear() {
