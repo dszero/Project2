b68e9878471a42156e94416f6edb89ed186e3344
David Thames <davidct@vt.edu>
2017-10-21 05:50:12 -0400

    Began outlining Quadtree test cases

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -1,4 +1,38 @@
+import junit.framework.TestCase;
 
-public class PRQuadTreeTest {
-	//TODO
+public class PRQuadTreeTest extends TestCase {
+	
+	PRQuadTree<Point> tree;
+	
+	public void setUp() {
+		tree = new PRQuadTree<Point>(0, 0, 1024, 1024);
+	}
+	
+	public void testInsert() {
+		//insert null (false)
+		
+		//insert valid point as head
+		
+		//insert valid point (convert leaf -> internal node)
+		
+		//insert valid point with same coord (without decomp)
+		//insert valid point with same coord (without decomp)
+		//insert valid point with same coord (without decomp)
+		
+		//insert valid point with diff coord (decomp)
+		
+		//insert valid point (internal node insertion) 
+	}
+	
+	public void testRemove() {
+		
+	}
+	
+	public void testFind() {
+		
+	}
+	
+	public void testClear() {
+		
+	}
 }
