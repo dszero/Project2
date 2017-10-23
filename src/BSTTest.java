d4bf030537d46386dc026c24bb097e97a57beff4
Shan Ding <dszero@vt.edu>
2017-10-22 20:11:27 -0400

    bst test

--- src/BSTTest.java
+++ src/BSTTest.java
@@ -14,8 +14,15 @@ public class BSTTest extends TestCase {
 	 */
 	public void testFindPoint()
 	{
-		
-		//bst.insert(x)
+		Point point = new Point("name", 1, 4);
+		Point point1 = new Point("apple", 4, 5);
+		Point point2 = new Point("zoo", 3, 7);
+		assertNull(bst.find(point));
+		bst.insert(point);
+		bst.insert(point2);
+		bst.insert(point1);
+		assertNotNull(bst.find(point2));
+		assertNotNull(bst.find(point1));
 	}
 
 }
